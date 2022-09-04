49ff1afcdbea924514143a5df6c810b48db7c477
edwardfox <edward.fox2006@gamil.com>
2022-09-04 04:35:38 +0100

    commit modified

--- Basalamtraining/src/cart/cart.jsx
+++ Basalamtraining/src/cart/cart.jsx
@@ -5,7 +5,7 @@ import './cart.css'
 const Cart = (props) => {
     return (
         <div className='container d-flex flex-column justify-content-center align-items-center bg-Homepage-container p-3'>
-            <div className='IMage-container mx-auto'>
+            <div className='IMage-container mx-auto d-flex justify-content-center align-items-start'>
                 <img className='w-100 h-100' src={props.image}/>
             </div>
             <div className='w-20 h-20'>
